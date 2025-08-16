36ba7966e06b47753742ac6c26d70aa665af3ac9
MATIAS ROSSELLO SCHEERER <itzmatidbz@gmail.com>
2025-08-15 22:55:11 -0300

    Corregir URL  de Supabase

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // app.js
 
 // 1. Configuración del cliente de Supabase
-const SUPABASE_URL = 'URL_DE_TU_PROYECTO'; // ¡Pega tu URL aquí!
-const SUPABASE_ANON_KEY = 'TU_ANON_KEY'; // ¡Pega tu clave anónima aquí!
+const SUPABASE_URL = 'https://fmsysdjqcliuwjesilam.supabase.co'; // ¡Pega tu URL aquí!
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtc3lzZGpxY2xpdXdqZXNpbGFtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMDYzNTYsImV4cCI6MjA3MDg4MjM1Nn0.PJpuqtAAnP5396wzP-g4Bh2tFs_NWjJ6YgyQiTVcx5w'; // ¡Pega tu clave anónima aquí!
 
 // --- INICIA LA CORRECCIÓN ---
 // Usamos el objeto global 'supabase' (del CDN) para crear nuestro cliente
